8241d9d7297b06ab60985371fb2f12b5b52b891c
alam-yi <1042697730@qq.com>
2023-03-15 10:49:41 +0800

    hcgc api demo

---
+++ hcgc/hcgc/Program.cs
@@ -0,0 +1,29 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// ≈‰÷√EF
+//builder.Services.AddDbContext < "YOUR_DBCONTEXT" > (option =>
+//    option.UseSqlServer(builder.Configuration.GetConnectionString(CONNECTION_STRING)));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
